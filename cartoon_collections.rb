18960b7715e19dcf2e16ab62ac559cce5a9987bc
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-25 16:19:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
-    
+    cheese_types.include? (item)
 end
