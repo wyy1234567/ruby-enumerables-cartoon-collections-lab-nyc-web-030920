0ed632f126d7290dd0b758e13a6c2e22bc1a0afe
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-25 16:01:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(array)
-  array.each_with_index {|item, index|}
+  array.each_with_index {|item, index|
+    puts array[index]
+  }
 end
 
 def summon_captain_planet# code an argument here
