18bc1b77e6080c0f4497cf4f5348062123c01034
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-25 16:20:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|item, index|
+    puts "/#{index + 1}.*#{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.map{|item| item.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|item| item.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include?(item)
+  end
 end
